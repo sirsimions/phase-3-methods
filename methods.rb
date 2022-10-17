a065e3e72d851a42f7db123f565e87c11efc63d0
Simion <simion.okola@student.moringaschool.com>
2022-10-17 12:12:25 +0300

    Completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,19 @@
 # Your code here!
+def greet_programmer()
+    puts "Hello, programmer!"
+end
+def greet (name)
+    puts "Hello, #{name}!"
+end
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end
+def add (val1, val2)
+    return val1 + val2
+end
+def halve (num)
+    if num.class != Integer
+        return nil
+    end
+    num/2
+end
